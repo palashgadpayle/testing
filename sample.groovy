0c1e04afa3421c6cd35b509225956b925d49492a
nishant <nyindorkar@gmail.com>
2023-01-17 16:41:33 +0530

    add commands

--- sample.groovy
+++ sample.groovy
@@ -4,16 +4,19 @@ pipeline {
         stage('git-pull') {
             steps { 
                 echo 'git pull successful'
+                sh 'echo $ENV'
             }
         }
         stage('build') {
             steps { 
                 echo 'build successful'
+                sh 'ls'
             }
         }
         stage('testing') {
             steps { 
                 echo 'testing successful'
+                sh 'pwd'
             }
         }
         stage('deploying') {
