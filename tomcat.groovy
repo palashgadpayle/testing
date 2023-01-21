d7187c58bedff5e6cd4ef46432c6b8733ae5c050
nishant <nyindorkar@gmail.com>
2023-01-21 16:36:41 +0530

    final

--- tomcat.groovy
+++ tomcat.groovy
@@ -24,23 +24,27 @@ pipeline {
                 sh 'aws s3 cp **/*.war s3://studentngpbckt/student-${BUILD_ID}.war'
             }
         }
-        // stage('tomcat-build') {
-        //     steps { 
-        //         sh '''
-        //         ssh -i ${tomcat}  -o StrictHostKeyChecking=no ubuntu@44.206.227.6<<EOF
-        //         aws s3 cp s3://myawwbucket/student-${BUILD_ID}.war /opt/
-        //         sudo wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz'
-        //         sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
-        //         sudo cp -rv student-${BUILD_ID}.war studentapp.war
-        //         sudo cp -rv studentapp.war /opt/apache-tomcat-8.5.82/webapps/
-        //         sudo ./opt/apache-tomcat-8.5.82/bin/startup.sh
-        //         '''
-        //     }
-        // }
-        // stage('deploying') {
-        //     steps { 
-        //         echo 'deploy completed'
-        //     }
-        //}
+        stage('tomcat-build') {
+            steps { 
+                withCredentials([sshUserPrivateKey(), sshUserPrivateKey(credentialsId: 'cat', keyFileVariable: 'tomcat')]) 
+                {
+                sh '''
+                ssh -i ${tomcat} -o StrictHostKeyChecking=no ubuntu@3.84.10.201<<EOF
+                sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
+                sh 'unzip awscliv2.zip'
+                sh 'sudo ./aws/install'
+                aws s3 cp s3://studentngpbckt/student-${BUILD_ID}.war .
+                sudo apt-get update -y
+                sudo apt install openjdk-11-jdk -y
+                curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz'
+                sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
+                sudo sh /opt/apache-tomcat-8.5.85/bin/shutdown.sh
+                sudo cp -rv student-${BUILD_ID}.war studentapp.war
+                sudo cp -rv studentapp.war /opt/apache-tomcat-8.5.85/webapps/
+                sudo ./opt/apache-tomcat-8.5.85/bin/startup.sh
+                '''
+                }
+            }
+        }
     }
 }
